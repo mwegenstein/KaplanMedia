a8d083c423b72643dc7b16f752da0930130a7b05
Michelle Wegenstein <mwegenstein@gmail.com>
2013-11-10 14:53:54 -0800

    contact page, clients page
    added small details: cleaned up image folder, put new image w.out CNN
    logo on homepage, added captions to about image slider, styled slider

---
+++ app/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Settings
+---------------------*/
+$email_to = 'mwegenstein@gmail.com'; // Email addresses to send data. Seperate email addresses with ','
+$site_title = 'Kaplan Media Partners'; // Your website title
+$webmaster_name = "Rick Kaplan"; // Your name
+
+$headerbar_color = "#313131"; // Color of the headerbar in the email sent
+$form_type = "contact form"; // Type of form - default is Contact Form
+
+$success_redirect = "index.html"; // Page to redirect to after submission of form.
+
+?>
